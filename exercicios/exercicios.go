78e37078f996e95850349084813df3c79cdce29d
BrunaSaNu <bruna.santana.0511@gmail.com>
2025-02-21 05:00:07 -0300

    Exercices for fixing ponteiros

--- exercicios/exercicios.go
+++ exercicios/exercicios.go
@@ -3,16 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	variavelInteira := 10
+	//variavelInteira := 10
 
+	//var ponteiro * int
+	//ponteiro = &variavelInteira
+
+	//fmt.Println(*ponteiro)
+
+	//variavelInteira++
+
+	//*ponteiro = 30
+
+	//fmt.Println(*ponteiro)
+
+	idade := 25
 	var ponteiro * int
-	ponteiro = &variavelInteira
+	ponteiro = &idade
 
 	fmt.Println(*ponteiro)
 
-	variavelInteira++
-
-	*ponteiro = 30
+	*ponteiro = 26
 
 	fmt.Println(*ponteiro)
 
